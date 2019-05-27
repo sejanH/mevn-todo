781c32909fc6c0534ab4e947da39d665a147b868
Mominul Haque Sejan <sejan840@gmail.com>
2019-05-27 19:24:58 +0600

    add bootstrap jquery axios

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar navbar-expand-sm navbar-dark bg-info">
+      <!-- <a class="navbar-brand" href="#">Home</a> -->
+      <router-link class="navbar-brand" to="/">Home</router-link>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <!-- <a class="nav-link" href="/">
+              Home
+              <span class="sr-only">(current)</span>
+            </a>-->
+            <router-link class="nav-link" to="home">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="#">Link</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >Dropdown</a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container-fluid">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+  name: "app",
+  components: {}
+};
 </script>
-
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+html,
+body {
+  margin: 0;
+}
+
+.nav-item > .router-link-exact-active {
+  padding: 0.45rem 1rem;
+  border-bottom: 2px solid ghostwhite;
+}
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,41 @@
 import Vue from 'vue'
+import Router from 'vue-router';
 import App from './App.vue'
-
+window.jQuery = window.$ = require('jquery')
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+Vue.use(Router);
 Vue.config.productionTip = false
+/* Components */
+import Home from './components/Home'
+const routes = [
+  {
+    name: 'home', path: '/home', component: Home, meta: { requiresAuth: true }
+  }
+];
+
+const router = new Router({
+  mode: 'history',
+  routes
+});
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // this route requires auth, check if logged in
+    // if not, redirect to login page.
+    if (!auth.loggedIn()) {
+      next({
+        path: '/login',
+        query: { redirect: to.fullPath }
+      })
+    } else {
+      next()
+    }
+  } else {
+    next() // make sure to always call next()!
+  }
+})
 
 new Vue({
+  router: router,
   render: h => h(App),
 }).$mount('#app')
